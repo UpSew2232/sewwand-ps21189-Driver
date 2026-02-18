e39bda9d13fed3e0e9d3b7e1347c3dd47e9f7566
UpSew2232 <upekshasewwandi017@gmail.com>
2026-02-18 22:27:03 +0530

    info run frontend

---
+++ client.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const baseURL = import.meta?.env?.VITE_API_BASE_URL || 'http://localhost:8081';
+
+const client = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default client;
